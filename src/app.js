f9fccc9bd2865aa6ab39d3efbe6446cae1b71628
MacBook Pro <change@MacBook-Pro.local>
2016-10-18 15:01:19 +0800

    update cell

--- src/app.js
+++ src/app.js
@@ -8,15 +8,16 @@ var MainScene = cc.Layer.extend({
         var h = winSize.height;
         this._tableOutSize = cc.size(w, 1115);
         this._cellOutSize = cc.size(w, 340);
+        this._cellBigSize = cc.size(w, 2320);
+        this._cellInSize = cc.size(w, 220);
 
 
         this._tableOut = null;
-        this._tableIn = null;
-        this._data = [1];
+        this._data = [1, 1];
 
         this._delegate = null;
 
-        this._isOut = true;
+        this._isOnClickIndex = -1;
 
 
         this.init();
@@ -33,7 +34,7 @@ var MainScene = cc.Layer.extend({
         return false;
 
     },
-    _createOutTable:function () {
+    _createOutTable: function () {
         var table = this._tableOut = new cc.TableView(this, this._tableOutSize);
         table.setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL);
         table.setPosition(cc.p(0, 0));
@@ -41,6 +42,79 @@ var MainScene = cc.Layer.extend({
         table.setVerticalFillOrder(cc.TABLEVIEW_FILL_TOPDOWN);
         this.addChild(table);
         table.reloadData();
+    },
+
+    initTableCell: function (cell, isNormal) {
+        var size = this._cellOutSize;
+        if (!isNormal) {
+            size = this._cellBigSize;
+        }
+        var w = size.width;
+        var h = size.height;
+        var posy = h * 0.5;
+
+
+        var nameLab = cell.lzName;
+        var rankLab = cell.lzRank;
+        var cards = cell.lzCards;
+        var descLab = cell.lzDesc;
+        var lzAvatar = cell.lzAvatar;
+        var btnLab = cell.lzLabelBtn;
+        var bgLayer = cell.bgLayer;
+
+
+        nameLab.setPosition(10, h - 46);
+        rankLab.setPosition(10, h - 86);
+        descLab.setPosition(w * 0.75, h - 47);
+        lzAvatar.setPosition(67, h - 190);
+
+        var posX = [148, 192, 265, 310, 357, 416, 473];
+        for (var i = 0; i < 7; i++) {
+            cards[i].setPosition(posX[i], h - 190);
+        }
+        btnLab.setPosition(75, 43);
+
+        if (!isNormal) {
+            var node = new cc.LayerColor(cc.color(255, 255, 255, 255), w, 1980);
+            cell.addChild(node);
+            node.setPosition(0, 76);
+
+            for (var i = 0;i < 9; i++){
+                var cnode = this._createExtraCell();
+                cnode.setPosition(0,220 * i);
+                node.addChild(cnode);
+            }
+
+        }
+
+    },
+
+    _createExtraCell:function () {
+        var cell = new cc.Node();
+        var cellSize = this._cellInSize;
+
+        var w = cellSize.width;
+        var h = cellSize.height;
+        var posy = h * 0.5;
+
+        cell.setContentSize(cellSize);
+
+
+        var nameLabel = cell.lzName = new cc.LabelTTF("aaaaa", "Arial", 30);
+        nameLabel.setPosition(61, h * 0.84);
+        nameLabel.setFontFillColor(cc.color(225, 87, 12));
+        cell.addChild(nameLabel);
+
+        cell.lzCards = [];
+        for (var i = 0; i < 2; i++) {
+            var card = cell.lzCards[i] = new cc.Sprite("#poker01.png");
+            card.setPosition(150 + i * 44, posy);
+            cell.addChild(card);
+        }
+
+        return cell;
+
+
     },
 
 
@@ -53,65 +127,65 @@ var MainScene = cc.Layer.extend({
         if (!cell) {
             cell = new cc.TableViewCell();
 
-            var colorLayer = new cc.LayerColor(cc.color.WHITE, w, h - 20);
+            var colorLayer = cell.bgLayer = new cc.LayerColor(cc.color.WHITE, w, h - 20);
             cell.addChild(colorLayer);
             colorLayer.setPosition(0, 10);
 
             var nameLabel = cell.lzName = new cc.LabelTTF("aaaaa", "Arial", 30);
-            nameLabel.setPosition(10, h * 0.84);
+
             nameLabel.setAnchorPoint(0, 0.5);
             nameLabel.setFontFillColor(cc.color(225, 87, 12));
             colorLayer.addChild(nameLabel);
 
             var rank = cell.lzRank = new cc.LabelTTF("bbbbbb", "Arial", 20);
-            rank.setPosition(10, h * 0.7);
+
             rank.setAnchorPoint(0, 0.5);
             rank.setFontFillColor(cc.color(225, 87, 12));
             colorLayer.addChild(rank);
 
             var desc = cell.lzDesc = new cc.LabelTTF("cccccc", "Arial", 20);
-            desc.setPosition(w * 0.75, h * 0.85);
+
             desc.setAnchorPoint(0, 0.5);
             desc.setFontFillColor(cc.color(225, 87, 12));
             colorLayer.addChild(desc);
 
 
             var avatar = cell.lzAvatar = new cc.Sprite("#poker_back.png");
-            avatar.setPosition(67, posy);
+
             cell.addChild(avatar);
 
-            var posX = [148, 192, 265, 310, 357, 416, 473];
 
             cell.lzCards = [];
 
             for (var i = 0; i < 7; i++) {
                 var card = cell.lzCards[i] = new cc.Sprite("#poker01.png");
-                card.setPosition(posX[i], posy);
                 cell.addChild(card);
             }
 
-
             var lbBtn = cell.lzLabelBtn = new cc.LabelTTF("click", "Arial", 30);
-            lbBtn.setPosition(75, 43);
+
             lbBtn.setFontFillColor(cc.color(225, 87, 12));
             colorLayer.addChild(lbBtn);
 
-
+            this.initTableCell(cell, true);
         }
         return cell;
     },
-    scrollViewDidScroll:function (view) {
+    scrollViewDidScroll: function (view) {
     },
-    scrollViewDidZoom:function (view) {
+    scrollViewDidZoom: function (view) {
     },
 
     tableCellSizeForIndex: function (table, idx) {
+        if (idx == this._isOnClickIndex) {
+            return this._cellBigSize;
+        }
         return this._cellOutSize;
     },
     numberOfCellsInTableView: function (table) {
         return this._data.length;
     },
-    tableCellTouched:function (table, cell ,touch) {
+    tableCellTouched: function (table, cell, touch) {
         var loc = cell.convertTouchToNodeSpace(touch);
         var index = cell.getIdx();
 
@@ -119,14 +193,34 @@ var MainScene = cc.Layer.extend({
         var w = this._cellOutSize.width;
         var data = this._data[index];
         var rect = cc.rect(0, 0, 300, 80);
+
         if (cc.rectContainsPoint(rect, loc)) {
-            if(this._tableOut){
-                this._tableOut.removeAllChildrenWithCleanup(true);
-                this._tableOut.removeAllChildren();
+            if (index != this._isOnClickIndex) {
+                this._isOnClickIndex = index;
+                cc.log("fuck click index is " + index);
+                cell.bgLayer.setContentSize(this._cellBigSize.width, this._cellBigSize.height - 20);
 
-                cc.log("fuck index is in in in ");
-                this._delegate = new MainScene.delegateLayer();
-                this.addChild(this._delegate);
+                this.initTableCell(cell, false);
+
+            } else {
+                // cell.lzName.setPosition(cc.p(200,0));
+                cell.bgLayer.setContentSize(this._cellOutSize.width, this._cellOutSize.height - 20);
+                this._isOnClickIndex = -1;
+                this.initTableCell(cell, true);
+
+            }
+
+
+            this._tableOut.reloadData();
+
+
+            if (this._tableOut) {
+                // this._tableOut.removeAllChildrenWithCleanup(true);
+                // this._tableOut.removeAllChildren();
+
+                // cc.log("fuck index is in in in ");
+                // this._delegate = new MainScene.delegateLayer();
+                // this.addChild(this._delegate);
             }
         }
 
@@ -135,74 +229,6 @@ var MainScene = cc.Layer.extend({
 
 });
 
-
-MainScene.delegateLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-        var winSize = cc.winSize;
-        var w = winSize.width;
-        var h = winSize.height;
-        this._tableOutSize = cc.size(w, 1115);
-        this._cellInSize = cc.size(w, 270);
-
-
-        this._data = [1, 1, 1, 1, 1, 1];
-
-        this.init();
-
-    },
-    init:function () {
-        if (this._super()){
-            var table  =  new cc.TableView(this, this._tableOutSize);
-            table.setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL);
-            table.setPosition(cc.p(0, 0));
-            table.setDelegate(this);
-            table.setVerticalFillOrder(cc.TABLEVIEW_FILL_TOPDOWN);
-            this.addChild(table);
-            table.reloadData();
-            return true;
-        }
-        return false;
-
-    },
-    tableCellAtIndex: function (table, index) {
-        var cell = table.dequeueCell();
-        var size = this._cellInSize;
-        var w = size.width;
-        var h = size.height;
-        var posy = h * 0.5;
-        if (!cell) {
-            cell = new cc.TableViewCell();
-            var colorLayer = new cc.LayerColor(cc.color.WHITE, w, h);
-            cell.addChild(colorLayer);
-
-            var nameLabel = cell.lzName = new cc.LabelTTF("aaaaa", "Arial", 30);
-            nameLabel.setPosition(61, h * 0.84);
-            nameLabel.setFontFillColor(cc.color(225, 87, 12));
-            colorLayer.addChild(nameLabel);
-
-            cell.lzCards = [];
-            for (var i = 0; i < 2; i++) {
-                var card = cell.lzCards[i] = new cc.Sprite("#poker01.png");
-                card.setPosition(150 + i * 44, posy);
-                cell.addChild(card);
-            }
-
-        }
-        return cell;
-    },
-    scrollViewDidScroll:function (view) {
-    },
-    scrollViewDidZoom:function (view) {
-    },
-
-    tableCellSizeForIndex: function (table, idx) {
-        return this._cellInSize;
-    },
-    numberOfCellsInTableView: function (table) {
-        return 4;
-    }
-});
 
 MainScene.createScene = function () {
     var scene = new cc.Scene();
